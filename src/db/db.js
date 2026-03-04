276ff8fbc277706d291988b770f2cb3ee5c5c4b3
Fa Ainama Caldera S <Facalder>
2026-03-04 22:05:51 +0700

    feat: init configuration for drizzle and some of shared configuration

---
+++ src/db/db.js
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { result } from "@/shared/config/env";
+
+const db = drizzle(result.data?.DATABASE_URL || "", {
+  logger: result.data?.NODE_ENV === "development",
+});
+
+export default db;
---
+++ src/shared/utils/api-response.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { STATUS_CODES } from "@/shared/constants/status-code";
+
+export class ApiResponse {
+  constructor({ success, message, statusCode, data = null, errors }) {
+    this.success = success;
+    this.message = message;
+    this.statusCode = statusCode;
+    this.data = data;
+    this.errors = errors;
+  }
+
+  send() {
+    return NextResponse.json(
+      {
+        success: this.success,
+        message: this.message,
+        statusCode: this.statusCode,
+        ...(this.data !== undefined && { data: this.data }),
+        ...(this.errors !== undefined && { errors: this.errors }),
+      },
+      { status: this.statusCode },
+    );
+  }
+
+  static Success(message, data, statusCode = STATUS_CODES.OK) {
+    return new ApiResponse({ success: true, message, data, statusCode }).send();
+  }
+
+  static ok(message = "OK", data) {
+    return ApiResponse.Success(message, data, STATUS_CODES.OK);
+  }
+
+  static created(message = "Created", data) {
+    return ApiResponse.Success(message, data, STATUS_CODES.CREATED);
+  }
+
+  static error(
+    message,
+    statusCode = STATUS_CODES.INTERNAL_SERVER_ERROR,
+    errors,
+  ) {
+    return new ApiResponse({
+      success: false,
+      message,
+      statusCode,
+      errors,
+    }).send();
+  }
+}
